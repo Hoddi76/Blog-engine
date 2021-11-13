f7efef69745189a56b9ef982b932c70ecff2027f
Hoddi76 <godikos@yandex.ru>
2021-11-13 22:23:50 +0300

    Changed the structure of the application added posts

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+
 from config import Config
+from posts.blueprint import posts
 
 app = Flask(__name__, static_folder="static")
 app.config.from_object(Config)
-
+app.register_blueprint(posts, url_prefix='/post')
