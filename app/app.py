f98f4d102453799c68da2983a45e07579f731a15
Hoddi76 <godikos@yandex.ru>
2021-11-14 21:38:57 +0300

    A database has been added

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopConfig, ProductConfig
 from posts.blueprint import posts
 
+
 app = Flask(__name__, static_folder="static")
 app.config.from_object(DevelopConfig)
+db = SQLAlchemy(app)
+
 app.register_blueprint(posts, url_prefix='/post')
--- app/config.py
+++ app/config.py
@@ -1,6 +1,17 @@
-class DevelopConfig(object):
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
 
 
-class ProductConfig(object):
+class ProductConfig(BaseConfig):
     DEBUG = False
