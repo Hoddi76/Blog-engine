8e860d8efb03a1f75cea760642e0b5e695082138
Hoddi76 <godikos@yandex.ru>
2021-11-15 22:43:42 +0300

    Rename module posts in blog

--- app/app.py
+++ app/app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopConfig, ProductConfig
-from posts.blueprint import posts
+from blog.blueprint import blog
 
 
 app = Flask(__name__, static_folder="static")
 app.config.from_object(DevelopConfig)
 db = SQLAlchemy(app)
 
-app.register_blueprint(posts, url_prefix='/post')
+app.register_blueprint(blog, url_prefix='/blog')
--- app/posts/blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-posts = Blueprint('posts', __name__, template_folder='templates')
-
-
-@posts.route('/')
-def post():
-    return render_template('posts/index.html')
