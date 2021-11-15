dacda193d9e27ea264f3e0e983bf78c288ccae27
Hoddi76 <godikos@yandex.ru>
2021-11-15 23:12:31 +0300

    Удалена директория app
    Директория app вводила в заблуждение из за уже имеющегося модуля app. По этому директория была удалена

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopConfig, ProductConfig
-from blog.blueprint import blog
+
 
 
 app = Flask(__name__, static_folder="static")
 app.config.from_object(DevelopConfig)
 db = SQLAlchemy(app)
 
-app.register_blueprint(blog, url_prefix='/blog')
--- blog/blueprint.py
+++ blog/blueprint.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
 
+from models import Post
+
+
 blog = Blueprint('blog', __name__, template_folder='templates')
 
 
 @blog.route('/')
 def post():
-    return render_template('blog/index.html')
+    posts = Post.query.all()
+    return render_template('blog/index.html', posts=posts)
