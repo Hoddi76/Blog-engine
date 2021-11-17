d0fdffdb3f503fc6909f095f0b4f25d2bdd6e8e0
Hoddi76 <godikos@yandex.ru>
2021-11-17 20:56:40 +0300

    Added view post_detal

--- blog/blueprint.py
+++ blog/blueprint.py
@@ -2,11 +2,17 @@ from flask import Blueprint, render_template
 
 from models import Post
 
-
 blog = Blueprint('blog', __name__, template_folder='templates')
 
 
 @blog.route('/')
-def post():
-    posts = Post.query.all()
-    return render_template('blog/index.html', posts=posts)
+def posts():
+
+    return render_template('blog/index.html', posts=Post.query.all())
+
+
+@blog.route('/<slug>')
+def post_detail(slug):
+
+    post = Post.query.filter(Post.slug == slug).first()
+    return render_template('blog/post_detail.html', post=post)
