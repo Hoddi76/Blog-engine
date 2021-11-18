6db5d64aad0278b949aa311966ee58339791a62d
Hoddi76 <godikos@yandex.ru>
2021-11-18 11:10:54 +0300

    Added the possibility of database migration

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import DevelopConfig, ProductConfig
 
-
-
 app = Flask(__name__, static_folder="static")
 app.config.from_object(DevelopConfig)
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+import models
 
